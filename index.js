325d43f34d773d0a4705f5484360e7c6e79760d0
abodiamhe <akhigbesadiq@gmail.com>
2024-07-03 16:07:07 +0100

    add testId

--- index.js
+++ index.js
@@ -23,10 +23,8 @@ const slackProfilePicture = document.querySelector("img");
 slackProfilePicture.src = "asserts/images/profile-image.jpg";
 slackProfilePicture.alt = "Sadiq profile picture";
 
-const slackDisplayName = "Akhigbe Sadiq";
 const slackEmail = "akhigbesadiq@gmail.com";
 
-document.getElementById("user-name").innerHTML = slackDisplayName;
 document.getElementById("user-email").innerHTML = slackEmail;
 document.getElementById("user-day").innerHTML = currentDay;
 document.getElementById("span").innerHTML = new Date().getFullYear();
